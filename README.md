37d4bdef22868ceab2281d9b6aafc380eee603d8
jdiaz <jdiaz@xmltravelgate.com>
2018-04-18 12:20:44 +0200

    add bin as hidden input
    add last4 as hidden input

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# js-card-crypt
+# Swappt Checkout JS
+
+JavaScript file to encrypt a client card.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+Install [TypeScript](https://www.typescriptlang.org/) with NPM running `npm install -g typescript`.
+
+## Deployment
+
+Transpile .ts files into .js files with `tsc *.ts`.
+
+Import `jsencrypt.js` and `main.js` files in your HTML code.
+
+Generate a form like in the example from `index.html`.
+
+## Built With
+
+* [JSEncrypt](https://github.com/travist/jsencrypt) - JavaScript Encrypt Library
+* [TypeScript](https://www.typescriptlang.org/) - JavaScript Superset
+
+
---
+++ src/js/main.js
@@ -0,0 +1,63 @@
+"use strict";
+
+var Card = (function() {
+  function Card(number, expiration_month, expiration_year, security_code) {
+    this.number = number;
+    this.expiration_month = expiration_month;
+    this.expiration_year = expiration_year;
+    this.security_code = security_code;
+    this.bin = number.substr(0,6);
+    this.last4 = number.substr(-4)
+  }
+  return Card;
+})();
+
+(function() {
+  document.getElementById("card-button").addEventListener("click", function() {
+    event.preventDefault();
+    var number = document.getElementById("card-number").value;
+    var month = document.getElementById("card-month").value;
+    var year = document.getElementById("card-year").value;
+    var cvv = document.getElementById("card-cvv").value;
+    var card = new Card(number, month, year, cvv);
+
+    var publicKey = `-----BEGIN PUBLIC KEY-----     YOUR UBLIC KEY HERE          -----END PUBLIC KEY-----`;
+
+    var encrypt = new JSEncrypt();
+    encrypt.setPublicKey(publicKey);
+    var encrypted = encrypt.encrypt(JSON.stringify(card));
+
+    saveCard(encrypted, card.bin, card.last4);
+  });
+})();
+
+function saveCard(encrypted,bin,last4) {
+  var form = document.getElementById("card-form");
+  form.reset();
+  addHiddenElement(form, "card-token",encrypted)
+  addHiddenElement(form, "card-bin",bin)
+  addHiddenElement(form, "card-last4",last4)
+ 
+  //console.log(form)
+}
+
+function addHiddenElement(form, fieldID, value){
+  // if the field not exist
+  if ( !document.getElementById(fieldID)){
+    // add new hidden element
+    form.appendChild(createHiddenElement( fieldID, value))
+
+  }else{
+    // update the exist element value
+    document.getElementById(fieldID).value = value
+  }
+}
+
+function createHiddenElement( name, value ){
+  var hiddenInput = document.createElement("input");
+  hiddenInput.setAttribute("type", "hidden");
+  hiddenInput.setAttribute("name", name);
+  hiddenInput.setAttribute("id", name)
+  hiddenInput.setAttribute("value", value);
+  return hiddenInput
+}
